6607068c2395abcd1e1875349890cc10a3106338
aasplund@gmail.com <aasplund@gmail.com@d268caf7-81f9-6220-81b2-a4a8bc6d0205>
2010-10-17 06:13:46 +0000

    Refactor package structure

---
+++ trunk/core-bc/composites/types/src/test/java/se/vgregion/dao/domain/patterns/repository/inmemory/MockEntityRepository.java
@@ -0,0 +1,7 @@
+package se.vgregion.dao.domain.patterns.repository.inmemory;
+
+import se.vgregion.dao.domain.patterns.repository.Repository;
+
+public interface MockEntityRepository extends Repository<MockEntity, Integer> {
+
+}
---
+++ trunk/core-bc/composites/types/src/test/java/se/vgregion/dao/domain/patterns/valueobject/MockValueObject.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2010 Västra Götalandsregionen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
+ *
+ */
+package se.vgregion.dao.domain.patterns.valueobject;
+
+import java.io.Serializable;
+
+import se.vgregion.dao.domain.patterns.valueobject.AbstractValueObject;
+
+/**
+ * This action do that and that, if it has something special it is.
+ * 
+ * @author Anders Asplund - <a href="http://www.callistaenterprise.se">Callista Enterprise</a>
+ */
+
+public class MockValueObject extends AbstractValueObject<MockValueObject> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    protected MockValueObject() {
+        // Used by JPA
+    }
+
+    public MockValueObject(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}

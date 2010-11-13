54d61763e7aca924ba1a82d0adaceba85c2e8412
aasplund@gmail.com <aasplund@gmail.com@d268caf7-81f9-6220-81b2-a4a8bc6d0205>
2010-11-13 20:54:05 +0000

    Added a builder pattern interface for an entity to make it possible to keep them immutable

--- trunk/core-bc/composites/types-jpa/src/main/java/se/vgregion/dao/domain/patterns/repository/db/jpa/JpaRepository.java
+++ trunk/core-bc/composites/types-jpa/src/main/java/se/vgregion/dao/domain/patterns/repository/db/jpa/JpaRepository.java
@@ -22,9 +22,6 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
 import se.vgregion.dao.domain.patterns.entity.Entity;
 import se.vgregion.dao.domain.patterns.repository.db.DatabaseRepository;
 
@@ -40,16 +37,15 @@ import se.vgregion.dao.domain.patterns.repository.db.DatabaseRepository;
  * 
  * @author Anders Asplund - <a href="http://www.callistaenterprise.se">Callista Enterprise</a>
  */
-public interface JpaRepository<T extends Entity<T, ID>, ID extends Serializable, PK extends Serializable>
-        extends DatabaseRepository<T, ID, PK> {
+public interface JpaRepository<T extends Entity<T, ID>, ID extends Serializable, PK extends Serializable> extends
+        DatabaseRepository<T, ID, PK> {
 
     /**
-     * Clear the persistence context, causing all managed entities to
-     * become detached. Changes made to entities that have not been flushed to the storage will not be persisted.
+     * Clear the persistence context, causing all managed entities to become detached. Changes made to entities
+     * that have not been flushed to the storage will not be persisted.
      */
     void clear();
 
-    
     /**
      * Find instances of <code>T</code> that match the criteria defined by query <code>queryName</code>.
      * <code>args</code> provide the values for any named parameters in the query identified by
@@ -86,7 +82,7 @@ public interface JpaRepository<T extends Entity<T, ID>, ID extends Serializable,
      * @return T or null if no objects match the criteria if more than one instance is returned.
      */
     T findInstanceByNamedQuery(String queryName, Object[] args);
-    
+
     /**
      * Find a single instance of <code>T</code> using the query named <code>queryName</code> and the arguments
      * identified by <code>args</code>.
@@ -97,8 +93,6 @@ public interface JpaRepository<T extends Entity<T, ID>, ID extends Serializable,
      *            a Map holding the named parameters of the query
      * @return T or null if no objects match the criteria if more than one instance is returned.
      */
-    @Transactional(propagation = Propagation.REQUIRED, readOnly = true)
     T findInstanceByNamedQuery(String queryName, Map<String, ? extends Object> args);
-
 
 }

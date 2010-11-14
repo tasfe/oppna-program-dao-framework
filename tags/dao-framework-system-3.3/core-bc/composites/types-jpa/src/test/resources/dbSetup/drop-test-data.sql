86eacad67bdbeba9a618a4c74656e20002dfe5f2
aasplund@gmail.com <aasplund@gmail.com@d268caf7-81f9-6220-81b2-a4a8bc6d0205>
2010-11-14 22:19:26 +0000

    Removing invalid tag

--- tags/dao-framework-system-3.3/core-bc/composites/types-jpa/src/test/resources/dbSetup/drop-test-data.sql
+++
@@ -1,1 +0,0 @@
-delete from vgr_test_entity;
--- tags/dao-framework-system-3.3/core-bc/composites/types/src/test/java/se/vgregion/dao/domain/patterns/repository/inmemory/MockEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package se.vgregion.dao.domain.patterns.repository.inmemory;
-
-import se.vgregion.dao.domain.patterns.repository.Repository;
-
-public interface MockEntityRepository extends Repository<MockEntity, Integer> {
-
-}

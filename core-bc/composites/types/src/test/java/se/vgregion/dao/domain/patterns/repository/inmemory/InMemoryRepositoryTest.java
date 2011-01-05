6ef728cc107f77d1fb00703072d7e41460d2156f
aasplund@gmail.com <aasplund@gmail.com@d268caf7-81f9-6220-81b2-a4a8bc6d0205>
2011-01-05 08:15:31 +0000

    Concretize AbstractInMemoryRepsository. Fixes issue 6

--- core-bc/composites/types/src/test/java/se/vgregion/dao/domain/patterns/repository/inmemory/InMemoryRepositoryTest.java
+++ core-bc/composites/types/src/test/java/se/vgregion/dao/domain/patterns/repository/inmemory/InMemoryRepositoryTest.java
@@ -21,44 +21,44 @@ package se.vgregion.dao.domain.patterns.repository.inmemory;
 import org.junit.Assert;
 import org.junit.Test;
 
+import se.vgregion.dao.domain.patterns.repository.Repository;
 
 public class InMemoryRepositoryTest {
 
-    private InMemoryMockRepository repository = new InMemoryMockRepository();
+    private Repository<MockEntity, Integer> repository = new InMemoryRepository<MockEntity, Integer>();
 
     @Test
     public void store() {
         repository.store(new MockEntity(123));
-        
+
         Assert.assertEquals(1, repository.findAll().size());
     }
 
     @Test
     public void persist() {
         repository.persist(new MockEntity(123));
-        
+
         Assert.assertEquals(1, repository.findAll().size());
     }
 
     @Test
     public void merge() {
         repository.merge(new MockEntity(123));
-        
+
         Assert.assertEquals(1, repository.findAll().size());
     }
 
-    
     @Test
     public void contains() {
         repository.store(new MockEntity(123));
-        
+
         Assert.assertTrue(repository.contains(new MockEntity(123)));
     }
 
     @Test
     public void find() {
         repository.store(new MockEntity(123));
-        
+
         Assert.assertEquals(new Integer(123), repository.find(123).getId());
     }
 
@@ -66,29 +66,29 @@ public class InMemoryRepositoryTest {
     public void findAll() {
         repository.store(new MockEntity(123));
         repository.store(new MockEntity(456));
-        
+
         Assert.assertEquals(2, repository.findAll().size());
     }
 
     @Test
     public void removeById() {
         repository.store(new MockEntity(123));
-        
+
         Assert.assertEquals(1, repository.findAll().size());
-        
+
         repository.remove(123);
-        
+
         Assert.assertEquals(0, repository.findAll().size());
     }
 
     @Test
     public void removeByEntity() {
         repository.store(new MockEntity(123));
-        
+
         Assert.assertEquals(1, repository.findAll().size());
-        
+
         repository.remove(new MockEntity(123));
-        
+
         Assert.assertEquals(0, repository.findAll().size());
     }
 

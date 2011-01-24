4110599c6f4f476576d14b39f24cc3defa48a17e
aasplund@gmail.com <aasplund@gmail.com@d268caf7-81f9-6220-81b2-a4a8bc6d0205>
2011-01-24 16:05:06 +0000

    Reverting previous commit and going back to revision 88

--- trunk/core-bc/composites/types-jpa/src/main/java/se/vgregion/dao/domain/patterns/repository/db/jpa/AbstractJpaRepository.java
+++ trunk/core-bc/composites/types-jpa/src/main/java/se/vgregion/dao/domain/patterns/repository/db/jpa/AbstractJpaRepository.java
@@ -20,13 +20,7 @@
 package se.vgregion.dao.domain.patterns.repository.db.jpa;
 
 import java.io.Serializable;
-import java.lang.reflect.Array;
-import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.lang.reflect.TypeVariable;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -64,103 +58,21 @@ public abstract class AbstractJpaRepository<T extends Entity<ID>, ID extends Ser
     @PersistenceContext
     protected EntityManager entityManager;
 
-    private Class<? extends T> type;
+    /**
+     * Entity class type.
+     */
+    protected Class<? extends T> type;
 
     public void setType(Class<? extends T> type) {
         this.type = type;
     }
 
     /**
-     * Entity class type.
-     */
-    public Class<? extends T> getType() {
-        return type;
-    }
-
-    /**
      * Default constructor.
      */
-    public AbstractJpaRepository() {
-        this.type = getTypeArguments(getClass()).get(0);
-    }
-
-    /**
-     * Get the underlying class for a type, or null if the type is a variable type.
-     * 
-     * @param type
-     *            the type
-     * @return the underlying class
-     */
     @SuppressWarnings("unchecked")
-    private Class<? extends T> getClass(Type type) {
-        if (type instanceof Class) {
-            return (Class<? extends T>) type;
-        } else if (type instanceof ParameterizedType) {
-            return getClass(((ParameterizedType) type).getRawType());
-        } else if (type instanceof GenericArrayType) {
-            Type componentType = ((GenericArrayType) type).getGenericComponentType();
-            Class<? extends T> componentClass = getClass(componentType);
-            if (componentClass != null) {
-                return (Class<? extends T>) Array.newInstance(componentClass, 0).getClass();
-            } else {
-                return null;
-            }
-        } else {
-            return null;
-        }
-    }
-
-    /**
-     * Get the actual type arguments a child class has used to extend a generic base class.
-     * 
-     * @param childClass
-     *            the child class
-     * @return a list of the raw classes for the actual type arguments.
-     * 
-     * @see http://www.artima.com/weblogs/viewpost.jsp?thread=208860
-     */
-    private List<Class<? extends T>> getTypeArguments(
-            @SuppressWarnings("rawtypes") Class<? extends AbstractJpaRepository> childClass) {
-        Map<Type, Type> resolvedTypes = new HashMap<Type, Type>();
-        Type type = childClass;
-        // start walking up the inheritance hierarchy until we hit this class
-        while (!getClass(type).equals(AbstractJpaRepository.class)) {
-            if (type instanceof Class) {
-                // there is no useful information for us in raw types, so just keep going.
-                type = ((Class<?>) type).getGenericSuperclass();
-            } else {
-                ParameterizedType parameterizedType = (ParameterizedType) type;
-                Class<?> rawType = (Class<?>) parameterizedType.getRawType();
-
-                Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
-                TypeVariable<?>[] typeParameters = rawType.getTypeParameters();
-                for (int i = 0; i < actualTypeArguments.length; i++) {
-                    resolvedTypes.put(typeParameters[i], actualTypeArguments[i]);
-                }
-
-                if (!rawType.equals(AbstractJpaRepository.class)) {
-                    type = rawType.getGenericSuperclass();
-                }
-            }
-        }
-
-        // finally, for each actual type argument provided to baseClass, determine (if possible)
-        // the raw class for that type argument.
-        Type[] actualTypeArguments;
-        if (type instanceof Class) {
-            actualTypeArguments = ((Class<?>) type).getTypeParameters();
-        } else {
-            actualTypeArguments = ((ParameterizedType) type).getActualTypeArguments();
-        }
-        List<Class<? extends T>> typeArgumentsAsClasses = new ArrayList<Class<? extends T>>();
-        // resolve types by chasing down type variables.
-        for (Type baseType : actualTypeArguments) {
-            while (resolvedTypes.containsKey(baseType)) {
-                baseType = resolvedTypes.get(baseType);
-            }
-            typeArgumentsAsClasses.add(getClass(baseType));
-        }
-        return typeArgumentsAsClasses;
+    public AbstractJpaRepository() {
+        this.type = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
     }
 
     /**
